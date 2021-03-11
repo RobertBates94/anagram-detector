4f673c92bd5278ca627c7248648d860f2f38744c
RobertBates94 <bates.to.infinity@gmail.com>
2021-03-10 22:57:31 -0600

    commit tree 3ae9bc15e4f42b421eab06379e1448cab522ec9b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+      
+      def initialize(word)
+        @word = word
+      end
+
+      def match(string)
+        string.select do |a|
+            a.split("").sort == word.split("").sort
+        end
+      end
+
+end
+
+
+
+
 # Your code goes here!
